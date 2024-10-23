4d1c9cbf7eed192f6d375edfb2c398b9586eab9d
Aranda999 <elias_aranda999@hotmail.com>
2024-10-22 19:52:19 -0600

    Login completo

--- api/Login/login_view.py
+++ api/Login/login_view.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -8,7 +9,7 @@ def login_views(request):
     template_view = "signin.html"
     
     #Verifica si el usuario ya esta logueado
-    if request.user.is_authenticated:
+    if request.user.is_authenticated and request.user.is_active:
         return redirect('home')
 
     if request.method == 'POST':
@@ -26,7 +27,32 @@ def login_views(request):
 #Vista para registro
 def register_view(request):
     template_view = "signup.html"
-    return render(request,template_name=template_view)
+    
+    if request.method == "POST":
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        
+        # Comprobar si el correo ya está en uso
+        if User.objects.filter(email=email).exists():
+            messages.error(request, "El correo ya existe.")
+        # Comprobar si el nombre de usuario ya está en uso
+        elif User.objects.filter(username=username).exists():
+            messages.error(request, "El nombre de usuario ya existe.")
+        else:
+            try:
+                user = User.objects.create_user(
+                    username=username,
+                    email=email,
+                    password=password,
+                    is_active = 0)
+                user.save()
+                messages.success(request, "Usuario registrado con éxito.")
+                return redirect('register_view')  # Redirige a la vista de login
+            except Exception as e:
+                messages.error(request, "Error al registrar el usuario:" + str(e))
+
+    return render(request, template_view)
 
 #Vista para salir
 def logout_view(request):
